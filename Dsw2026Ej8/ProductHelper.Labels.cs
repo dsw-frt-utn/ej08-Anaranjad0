397bd548afe60edb97b2bee73d316676c0f992a5
Tobias Juarez <tobiasjuarez20@gmail.com>
2026-05-04 09:59:54 -0300

    Resolución del problema 1

---
+++ Dsw2026Ej8/ProductHelper.Labels.cs
@@ -0,0 +1,7 @@
+﻿namespace Dsw2026Ej8
+{
+    public partial class ProductHelper
+    {
+        public const string CONSTANTE =  "[{0}] {1} - {2}";
+    }
+}
---
+++ Dsw2026Ej8/ProductHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace Dsw2026Ej8
+{
+    public partial class ProductHelper
+    {
+        public string GetProductLabel(long code, string description, decimal price)
+        {
+            return string.Format(CONSTANTE, code, description, price.ToString("C"));
+        }
+    }
+}
