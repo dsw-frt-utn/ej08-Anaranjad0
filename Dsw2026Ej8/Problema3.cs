d3421eef8927f1dca7508d7154e262c2534631e0
Tobias Juarez <tobiasjuarez20@gmail.com>
2026-05-04 10:31:05 -0300

    Resolucion del Problema 3

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,16 @@
+﻿namespace Dsw2026Ej8
+{
+    internal class Problema3
+    {
+        public string CompareCopies(int originalValue, Product product)
+        {
+            int copy = originalValue;
+            copy++;
+
+            Product productCopy = product;
+            productCopy.ModifyDescription("New description");
+
+            return $"{originalValue}-{copy}-{product.Description}";
+        }
+    }
+}
---
+++ Dsw2026Ej8/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace Dsw2026Ej8
+{
+    public class Product
+    {
+        public string Description { get; private set; }
+
+        public Product (string description)
+        {
+            Description = description;
+        }
+
+        public void ModifyDescription(string newDescription)
+        {
+            if (string.IsNullOrWhiteSpace(newDescription))
+            {
+                throw new ArgumentException("La descripción no puede ser vacía o nula.");
+            }
+            Description = newDescription;
+        }
+    }
+}
