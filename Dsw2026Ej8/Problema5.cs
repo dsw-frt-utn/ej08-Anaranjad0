49659ca6eb4e7144ac51da0929f8689f9e862ddf
Tobias Juarez <tobiasjuarez20@gmail.com>
2026-05-04 10:50:14 -0300

    Resolución del Problema 5

---
+++ Dsw2026Ej8/Problema5.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema5
+    {
+        public decimal GetFinalAmount(Sale sale)
+        {
+            return sale.CalculateTotal();
+        }
+
+    }
+}
