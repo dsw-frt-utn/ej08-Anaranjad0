d4a30306e2719ac03ac42f1be1d38608222a7abe
Tobias Juarez <tobiasjuarez20@gmail.com>
2026-05-04 10:15:43 -0300

    Resolución del problema 2

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,20 @@
+﻿namespace Dsw2026Ej8
+{
+    internal class Problema2
+    {
+        public  string CreateSalesSummary (long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+            decimal total = quantity <= 0 ? 0 : quantity * unitPrice;
+
+            var summary = new
+            {
+                Code = productCode,
+                Description = productDescription,
+                Quantity = quantity,
+                Total = total
+            };
+
+            return $"{summary.Code} - {summary.Description} - {summary.Total:C}";
+        }
+    }
+};
